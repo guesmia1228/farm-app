a8fbeaf855e3e43d17054643f55b6cdd29e52e6b
guesmia1228 <guesmiaabdelmadjid6@gmail.com>
2023-11-15 23:26:49 +0300

    fix security problems

--- src/main/java/com/farmdigital/nerddevs/controller/LoginController.java
+++ src/main/java/com/farmdigital/nerddevs/controller/LoginController.java
@@ -4,19 +4,21 @@ import com.farmdigital.nerddevs.service.UserRegistrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/super")
+
+@RequestMapping("/api/v1/farm_digital/super")
 public class LoginController {
 
     private  final UserRegistrationService userRegistrationService;
 
-    @PostMapping("/user")
+    @GetMapping("/user")
+    public String  findUser(){
+        return "anerico";
+    }
+    @PostMapping("/super")
     public ResponseEntity<?> registerUse(@RequestBody UserRegistrationDto user){
         String  res= userRegistrationService.saveUer(user);
 
